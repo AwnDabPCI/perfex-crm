3f22826ca2e903440911b5d2d55ab2039cf0baf8
Backup User <backup@project.com>
2025-11-25 18:06:01 +0500

    Add better logging and database wait to entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,18 +12,31 @@ php -r "
     \$dbName = getenv('APP_DB_NAME') ?: 'perfex_crm';
     \$encKey = getenv('APP_ENC_KEY') ?: 'default_encryption_key_change_in_production';
     
+    error_log('=== RENDER CONFIG GENERATION ===');
+    error_log('Base URL: ' . \$baseUrl);
+    error_log('DB Driver: ' . \$dbDriver);
+    error_log('DB Host: ' . \$dbHost);
+    error_log('DB Port: ' . \$dbPort);
+    error_log('DB Name: ' . \$dbName);
+    error_log('DB User: ' . \$dbUser);
+    
     \$config = file_get_contents('/var/www/html/application/config/app-config-sample.php');
     \$config = str_replace('[base_url]', \$baseUrl, \$config);
     \$config = str_replace('[db_hostname]', \$dbHost, \$config);
+    \$config = str_replace('[db_port]', \$dbPort, \$config);
     \$config = str_replace('[db_username]', \$dbUser, \$config);
     \$config = str_replace('[db_password]', \$dbPass, \$config);
     \$config = str_replace('[db_name]', \$dbName, \$config);
     \$config = str_replace('[encryption_key]', \$encKey, \$config);
     \$config = str_replace('[db_driver]', \$dbDriver, \$config);
-    \$config = str_replace('[db_port]', \$dbPort, \$config);
     file_put_contents('/var/www/html/application/config/app-config.php', \$config);
-    echo 'Runtime: Config file created from environment variables' . PHP_EOL;
-"
+    error_log('Config file created successfully');
+    error_log('=== CONFIG GENERATION COMPLETE ===');
+" 2>&1 | tee -a /var/log/php_errors.log
+
+echo 'Waiting for database...'
+sleep 2
 
 # Run the original docker-php-entrypoint
-exec docker-php-entrypoint "$@"
+exec docker-php-entrypoint \"$@\"
+" 2>&1 | tee -a /var/log/php_errors.log
